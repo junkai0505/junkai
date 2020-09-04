402c561034b66ce52447f3ced3ae497601a73e7b
junkai0505 <1728934432@qq.com>
2020-09-04 15:04:19 +0800

    hello world

---
+++ PanDuanYuJu.py
@@ -0,0 +1,176 @@
+# !/usr/bin/env python
+# -*-coding:utf8-*-
+# Project_name:pycharm
+# File_name:PanDuanYuJu.py
+# Author: junkai 
+# Time:2020年07月15日
+# if True:
+#     print("我是你爸爸")
+#     print("我真是你爹")
+
+# age = int(input("请输入年龄"))
+# if age >= 18:
+#     print ("欢迎光临")
+# else:
+#     print("请离开")
+
+# a = input("请输入性别")
+# if str(a) =="男":
+#     print (f"你的性别是{a}")
+#     print ("请进入男澡堂")
+# if str(a) =="女":
+#     print("请进入男澡堂")
+#     print ("女，请进入女澡堂")
+
+
+
+# if 2 == 3:
+#     print("a==3")
+# else:
+#     print("2!=3")
+
+# a = input("请输入性别")
+# if a == "男":
+#     print(f"你的性别是{a}，请进入男澡堂")
+# if a == "女":
+#     print(f"你的性别是{a}，请进入女澡堂")
+
+# a = input("请输入性别")
+# if a == "男":
+#     print (f"你的性别是{a},请进入男澡堂")
+# elif a =="女":
+#     print(f"你的性别是{a},请进入女澡堂")
+# else:
+#     print("请输入正确性别")
+# age = int(input("请输入年龄："))
+# if age < 18:
+#     print ("你是童工")
+# elif age >=18 and age <= 60:
+#     print ("你是合法工人")
+# elif age > 60:
+#     print ("你是退休工人")
+# else:
+#     print ("请输入正确年龄")
+
+# score = int(input("请输入成绩"))
+# if score > 90:
+#     print ("优秀")
+# elif score >= 80 and score <=90:
+#     print ("良好")
+# elif score >= 60 and score <= 70:
+#     print ("及格")
+# else:
+#     print("不及格")
+
+
+
+
+
+
+
+
+
+
+
+
+
+# score = input("请输入成绩")
+# # if int(score) > 90:
+# #     print ("优秀")
+# # elif int(score) >= 80 and int(score) <=90:
+# #     print ("良好")
+# # elif int(score) >= 60 and int(score) <= 70:
+# #     print ("及格")
+# # else:
+# #     print("不及格")
+
+
+# money = 1
+# sea = 2
+# if money == 1:
+#     print ("请上车")
+#     if sea == 2:
+#         print ("请坐下")
+#     else:
+#         print ("请离开")
+# else:
+#     print ("请站者")
+
+
+# dayaoshi = 1
+# xiaoyaoshi = 2
+
+# if dayaoshi == 1:
+#     print ("请进入博文")
+#     if xiaoyaoshi == 2:
+#         print ("请进入教室学习")
+#     else:
+#         print ("请在办公室等待")
+# else:
+#     print ("滚")
+# import random
+# a = random.randint(0,2)
+# print (a)
+# player = int (input("请出拳：0-石头，1-剪刀，2-布："))
+# if (player == 0) and (a == 1) or (player == 1) and (a == 2)\
+#         or (player == 2) and (a == 0):
+#     print ("你赢了")
+# elif a == player:
+#     print ("这把平局")
+# else:
+#     print ("电脑赢了")
+
+# import random
+# a = random.randint(0,100)
+# b = int(input("请输入数字"))
+
+# if b > a:
+#     print("请大一点")
+# elif b < a:
+#     print("请小一点")
+# elif b == a:
+#     print("猜对了")
+
+# a = 1
+# b = 2if
+# c = a if a > b else b
+# print(c)
+
+# age = int(input("请输入年龄"))
+# cc ="成年" if age >= 18 else "未成年"
+# print (cc)
+
+# sex = input("请输入性别")
+# cc = "请进入男澡堂" if sex =="男" else "请进入女澡堂"
+# print (cc)
+
+# a = 10
+# while a > 1:
+#     a -= 1 #a = a-1
+#     print ("我是你爹")
+
+
+#
+# i = 1
+# b = 0
+# while i <= 5:
+#       b += i
+#       i += 1
+# print (b)
+
+
+# i = 1
+# b = 0
+# while i <=100 and i%2 != 0:
+#     b += i
+#     i += 2
+# print (b)
+
+# username = input("请输入姓名")
+# passowrd = input("请输入密码")
+# if username == "杨俊凯" and passowrd == "123456":
+#     print ("欢迎光临")
+# else:
+#     print ("请滚开")
+
+print('hello world')
